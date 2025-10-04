99ee5e2ed21dd1215b6d2e94c4aaaa945fcdc4a6
aqjsh <aqj.shi@gmail.com>
2025-10-04 06:16:10 +0000

    init

--- position_manager.py
+++ position_manager.py
@@ -1,4 +1,74 @@
 from state import *
+
+
+transition_dict = {
+
+    "a": new_id,
+
+    "b": prediction,
+
+    "c": query,
+
+    "d": not_query,
+
+    "e": not_active_and_above_threshold,
+
+    "f": below_threshold,
+
+    "g": not_filled,
+
+    "h": filled,
+
+    "i": active_and_update_tsm,
+
+    "j": trigger_tsm,
+
+    "k": not_trigger_tsm
+
+}
+
+
+states_dict = {
+
+    0: "idle",
+
+    1: "price_stream",
+
+    2: "prediction_plane",
+
+    3: "top_pair",
+
+    4: "declare",
+
+    5: "active",
+
+    6: "exit"
+
+}
+
+
+# row = state, col  = function, (row, col) = new state
+
+transition_table = [
+
+    0:  [1,0,0, 0,0,0, 0,0,0, 0,0],
+
+    1:  [1,2,1, 1,1,1, 1,1,1, 1,1],
+
+    2:  [2,2,3, 1,2,2, 2,2,2, 2,2],
+
+    3:  [3,3,3, 3,4,1, 3,3,5, 3,3],
+
+    4:  [4,4,4, 4,4,4, 1,5,4, 4,4],
+
+    5:  [5,5,5, 5,5,5, 5,5,5, 6,1],
+
+    6:  [1,6,6, 6,6,6, 6,6,6, 6,6],  
+
+]
+
+
+
 if __name__ == "__main__":
     
     assets = ['AAPL']
